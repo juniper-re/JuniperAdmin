aec0e8611fbf767b1f7d193ce2160775e8c6b39a
Lucas Teng <lucasteng@icloud.com>
2024-03-17 14:47:54 -0400

    updates to text section

--- src/pages/WhiteLabel/index.tsx
+++ src/pages/WhiteLabel/index.tsx
@@ -7,6 +7,7 @@ import { type IBrandingContext } from './types/context'
 import { defaults } from './utilities/defaults'
 import { download } from './utilities/download'
 import { type IBrandingOverrides } from './types/brandingOverrides'
+import { Other } from './Other'
 
 export const BrandingOverrideContext = createContext<IBrandingContext>({ brandingOverrides: JSON.parse(JSON.stringify(defaults)) as IBrandingOverrides, setBrandingOverrides: () => {} })
 export const WhiteLabel: React.FC = () => {
@@ -17,6 +18,7 @@ export const WhiteLabel: React.FC = () => {
     <BrandingOverrideContext.Provider value={{ brandingOverrides, setBrandingOverrides }}>
       <div className='rounded-2xl bg-gray-100 p-10 pb-20 pl-20'>
         <h1 className='DM-Serif-Display text-5xl text-gray-400'>Theme Generator</h1>
+
         <div className="mt-10 rounded-3xl bg-white p-5 shadow-md">
           <Login />
         </div>
@@ -24,10 +26,14 @@ export const WhiteLabel: React.FC = () => {
         <div className="mt-10 rounded-3xl bg-white p-5 shadow-md">
           <MainApplication />
         </div>
+
+        <div className="mt-10 rounded-3xl bg-white p-5 shadow-md">
+          <Other />
+        </div>
       </div>
 
       <div className="fixed bottom-0 flex w-full items-center bg-white bg-opacity-90 p-3">
-        <Button variant='filled' onClick={() => { download() }} className='mx-4'>Save</Button>
+        <Button variant='filled' onClick={() => { download() }} className='mx-4'>Export</Button>
         <Button variant='outlined' onClick={() => { setBrandingOverrides(JSON.parse(JSON.stringify(defaults)) as IBrandingOverrides) }}>Clear</Button>
       </div>
     </BrandingOverrideContext.Provider>
